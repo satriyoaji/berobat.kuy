17be1d84a93dee8af9da9ad7ef2a7ecd0e53f67d
edyribowo <eribowo9@gmail.com>
2019-05-06 15:01:13 +0700

    update navbar

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -21,6 +21,7 @@ MyAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
@@ -31,19 +32,14 @@ MyAsset::register($this);
 <div class="wrap">
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  
   <a class="navbar-brand" href="#"><img src="../../assets/logo.png" alt="" class="img-responsive" width="200" height="55"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
 </nav>
+
+
 
     <div class="container">
         <?= Breadcrumbs::widget([
