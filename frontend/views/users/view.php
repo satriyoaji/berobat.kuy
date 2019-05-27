9dbff7bfcf64cf52862538cb71e9bc146676a3b5
edyribowo <eribowo9@gmail.com>
2019-05-27 15:05:30 +0700

    adding user view

--- frontend/views/users/view.php
+++ frontend/views/users/view.php
@@ -7,40 +7,31 @@ use yii\widgets\DetailView;
 /* @var $model frontend\models\Users */
 
 $this->title = $model->userId;
-$this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+
 \yii\web\YiiAsset::register($this);
 ?>
-<div class="users-view">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->userId], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->userId], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'userId',
-            'username',
-            'userNama',
-            'password',
-            'userEmail:email',
-            'userTelephone',
-            'userAlamat',
-            'userPekerjaan',
-            'userFoto',
-            'userTanggalLahir',
-            'userJenisKelamin',
-        ],
-    ]) ?>
-
+<br>
+<br>
+<div class="col-md-12">
+    <div class="text-right">
+        <a href=""><button type="button" class="btn btn-raised btn-info">EDIT</button></a>
+    </div>
+    <div class="row">
+        <div class="col-md-4 text-center">
+            <img src="../../assets/FOTO USER/download.png" alt="..." class="img-thumbnail">
+        </div> 
+        <div class="col-md-8">
+            <h1><b>Edy Ribowo</b></h1>
+            <p><i>@username</i></p>
+            <h6 style="color:#666768;">edyribowo@gmail.com</h6>
+            <h6>Laki - laki</h6>
+            <h6>081249078442</h6>
+            <h6><i>Jl. Ahmad Yani No. 17</i></h6>
+            <h6><b>30, Nopember 2000</b></h6>
+            <br>
+            <br>
+            <br>
+        </div>
+    </div>
+   
 </div>
